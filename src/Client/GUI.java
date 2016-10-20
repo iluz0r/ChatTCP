c1411880deba22b3388ea33c461e7b5646d89a3f
Angelo S. <boyangel90@hotmail.it>
2016-10-20 23:25:40 +0200

    Echo Server with GUI

--- src/Client/GUI.java
+++ src/Client/GUI.java
@@ -20,6 +20,8 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class GUI {
 
@@ -61,72 +63,83 @@ public class GUI {
 		frame.setBounds(100, 100, 374, 260);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JPanel loginPanel = new JPanel();
 		loginPanel.setBorder(new LineBorder(SystemColor.controlShadow));
 		loginPanel.setBounds(0, 0, 368, 69);
 		frame.getContentPane().add(loginPanel);
 		loginPanel.setLayout(null);
-		
+
 		userTextField = new JTextField();
 		userTextField.setBounds(87, 25, 120, 20);
 		loginPanel.add(userTextField);
 		userTextField.setColumns(10);
-		
+
 		JLabel loginLabel = new JLabel("Username:");
 		loginLabel.setBounds(10, 27, 67, 17);
 		loginPanel.add(loginLabel);
-		
+
 		JButton loginButton = new JButton("Login");
 		loginButton.addActionListener(new LoginButtonListener());
 		loginButton.setBounds(259, 25, 89, 20);
 		loginPanel.add(loginButton);
-		
+
 		JPanel chatPanel = new JPanel();
 		chatPanel.setBorder(new LineBorder(SystemColor.controlShadow));
 		chatPanel.setBounds(0, 68, 368, 133);
 		frame.getContentPane().add(chatPanel);
 		chatPanel.setLayout(new BorderLayout(0, 0));
-		
+
 		chatTextArea = new JTextArea();
 		chatTextArea.setEditable(false);
 		chatPanel.add(chatTextArea);
-		
+
 		JPanel textPanel = new JPanel();
 		textPanel.setBorder(new LineBorder(SystemColor.controlShadow));
 		textPanel.setBounds(0, 202, 368, 30);
 		frame.getContentPane().add(textPanel);
 		textPanel.setLayout(new BorderLayout(0, 0));
-		
+
 		messageTextField = new JTextField();
+		messageTextField.addKeyListener(new SendTextKeyListener());
 		textPanel.add(messageTextField);
 		messageTextField.setColumns(10);
 	}
-	
+
 	class LoginButtonListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			try {
-				Client client = new Client();
-				Socket socket = client.getSocket();
-				
-				PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"));
-				pw.println(userTextField.getText());
-				pw.flush();
-				
-				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(),"UTF-8"));
-				String answer = br.readLine();
-				chatTextArea.setText(answer);
-				socket.close();
-			} catch (UnknownHostException e1) {
-				e1.printStackTrace();
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			} 	
 		}
-		
-	}
-	
-}
 
+	}
+
+	class SendTextKeyListener extends KeyAdapter {
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+				try {
+					Client client = new Client();
+					Socket socket = client.getSocket();
+
+					PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"));
+					pw.println(messageTextField.getText());
+					pw.flush();
+
+					BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+					String answer = br.readLine();
+					chatTextArea.append(answer + "\n");
+					messageTextField.setText("");
+					socket.close();
+				} catch (UnknownHostException e1) {
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+}
