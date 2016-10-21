824e74ca4fa235f4b5a1668c1464c50744ce4604
Angelo <Angelo@Angelo-PC>
2016-10-21 10:23:18 +0200

    Fix

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -9,9 +9,9 @@ import java.io.UnsupportedEncodingException;
 import java.net.Socket;
 
 public class Server implements Runnable {
-	
+
 	private Socket socket;
-	
+
 	public Server(Socket socket) {
 		this.socket = socket;
 	}
@@ -21,20 +21,20 @@ public class Server implements Runnable {
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
 			String s = br.readLine();
-			
-			String user=null;
-			String password=null;
-			
-			if(s.contains("LOGIN:")){
+
+			String user = null;
+			String password = null;
+
+			if (s.contains("LOGIN:")) {
 				user = s.split(":")[1];
 				password = s.split(":")[2];
 			}
-			
-			System.out.println(user+" "+password);
-				
+
+			System.out.println(user + " " + password);
+
 			PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"));
 			pw.println(s);
-			
+
 			// Svuota i buffer, forzando l'invio dei dati
 			pw.flush();
 		} catch (UnsupportedEncodingException e) {
