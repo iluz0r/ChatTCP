7cd224930a403d0f691831139dc7fe1f17c43b51
Angelo S. <boyangel90@hotmail.it>
2016-10-22 11:42:33 +0200

    Code clean

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -3,15 +3,12 @@ package Server;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
 import java.net.Socket;
 
 public class Server implements Runnable {
